c4baf5e3fe04ac0b57159eb2f6e683eaafcb2197
Kevin <trupiakevin@gmail.com>
2025-12-09 22:49:08 +0100

    feat: completata 3  task

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,24 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function numberVocals(str) {
+   
+    const vocals = ["a", "e", "i", "o", "u"];
+    let count = 0
+
+
+    for (let i = 0; i< str.length; i++) {
+        if (vocals.includes(str[i])) {
+            count++;
+        }
+    }
+
+    return count
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(numberVocals(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
