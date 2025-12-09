57384bcefc9e32c98522e771d8292455c05761d8
Kevin <trupiakevin@gmail.com>
2025-12-09 22:20:45 +0100

    feat: completata la terza task

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,4 +1,5 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo 
+le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
@@ -6,8 +7,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 // Dichiara la funzione qui.
 
 
+function Letters(nomi, lettera) {
+  let letters = [];
+
+  for (let i = 0; i < nomi.length; i++) {
+    if (nomi[i][0].toLowerCase() === lettera.toLowerCase()) {
+      letters.push(nomi[i]);
+    }
+  }
+
+  return letters;
+}
+
+
+
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(Letters(names, 'A'))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
