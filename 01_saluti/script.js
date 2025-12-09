fd76a6535a7922900892795af86325fb8259af0a
Kevin <trupiakevin@gmail.com>
2025-12-09 16:56:39 +0100

    feat: prima task completata

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,24 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+/* Scrivi una funzione che accetti una stringa contenente un nome
+e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
 
+function hello(name) {
+    return 'Ciao ' + name;
+}
+
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
+const saluto = hello(userName)
+console.log(saluto)
 
 
+let ciao = hello('Kevin')
+console.log(ciao)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
